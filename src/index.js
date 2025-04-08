a7c339653a5cf987569bee5fd4b1d2a80387652c
dosite-dev <dositecyiza@gmail.com>
2025-04-08 23:28:46 +0200

    new changes in js file

---
+++ src/index.js
@@ -0,0 +1,4 @@
+var name_error=document.getElementById('full-name')
+var phone_error=document.getElementById('telephone')
+var level_error=document.getElementById('level')
+var email_error=document.getElementById('email-error')
